4914c23bdbe752228b0a1973f2b46ba9c4189705
gleitons <gleitons@icloud.com>
2021-03-26 09:18:06 -0300

    criação de paginas

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.menu}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="/sobre">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
