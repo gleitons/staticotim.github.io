3e6765eb7c2cc58b76a5c9f2a6d1b61ee6bb099f
gleitons <31367377+gleitons@users.noreply.github.com>
2021-03-27 07:07:41 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.menu}>
         <h1 className={styles.title}>
-          Welcome to <a href="/sobre">Sobre!</a>
+          Welcome to <a href="/sobre.js">Sobre!</a>
         </h1>
 
         <p className={styles.description}>
