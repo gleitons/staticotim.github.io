d87f25dec68ef35e079959ca8018ca6d6e35f345
gleitons <31367377+gleitons@users.noreply.github.com>
2021-03-27 07:12:19 -0300

    Update contato.js

--- pages/contato.js
+++ pages/contato.js
@@ -4,6 +4,6 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div><title>PAGINA CONTATO</title></div>
-    <div><a href="/"INICIO></a></div>
+    <div><a href="/">INICIO</a></div>
   )
 }
